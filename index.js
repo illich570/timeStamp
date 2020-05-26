3b52932e781ba89c8d1ee688568852991a83c984
Illich Rada <illich570@gmail.com>
2020-05-26 19:04:55 -0400

    [ADD] Pug Install & Config addded

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const App = express();
+const path = require('path');
+
+
+App.set('views', path.join(__dirname, 'views'));
+App.use(express.static(__dirname + '/assets'));
+App.set("view engine", "pug");
 
 App.get('/', (req,res) =>{
-  res.send('Hi!');
+  res.render('index', {title: 'Illich', message: ' This is a message!'});
 })
 
 App.listen(process.env.PORT || 3000, () =>{
